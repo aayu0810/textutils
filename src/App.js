d2a7a793ce76605e8bec9bf9e25ecc26591323e2
Sukhdeep <sharma.sukhdeep30@gmail.com>
2023-07-24 11:28:33 -0400

    nav bar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 
 import './App.css';
+let name= "Aayu";
 
 function App() {
   return (<>
     
-    <div className="blank">
-     
-    </div>
+    <>
+    <nav>
+      <li>Home</li>
+      <li>About</li> 
+      <li>Contact</li>     
+       </nav></>
+       <div classname="container">
+        <h1> Hello {name} </h1>
+        <p> Bonjoure ! How are you guys?? Welcome to class with Aayu.
+          Hope you enjoy the rest of the session. 
+          </p>
+
+       </div>
     </>
   );
 }
