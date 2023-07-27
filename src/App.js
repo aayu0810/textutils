9b09aa58a66148dc5864b8d85e14ce05db7a752c
Sukhdeep <sharma.sukhdeep30@gmail.com>
2023-07-27 00:16:39 -0400

    router adding and usage

--- src/App.js
+++ src/App.js
@@ -3,8 +3,18 @@ import './App.css';
 
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import About from './components/About';
 import React, { useState } from 'react';
 import Alert from './components/Alert';
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
+
 
 function App() {
   const[mode,setMode] = useState('light');
@@ -41,16 +51,27 @@ else{
 
 
   return (
-    <>
-   <Navbar title ="Textutils" mode={mode} toggleMode={toggleMode}/>
+    <> 
+
+<Router>
+   <Navbar title ="Textutils" AboutText ="About " mode={mode} toggleMode={toggleMode}/>
    <Alert alert ={alert}/>
-    <div className= "container">
-    <TextForm showAlert={showAlert} heading ="ENTER THE TEXT TO ANALYZE BELOW" mode={mode}/>
-   {/* <Navbar title ="TextUtils" aboutText ="About TextUtile"/>*/}
+    <div className= "container my-3"> 
+    <Routes>
+    <Route exact path="/About" element={<About />}/>
+    <Route exact path="/" element={<TextForm showAlert={showAlert} heading ="ENTER THE TEXT TO ANALYZE BELOW" mode={mode}/>}/>
+    </Routes> 
     </div>
+ </Router>
    
+
+  
+    {/*<Navbar title ="TextUtils" aboutText ="About TextUtiles"/>*/}
+
+   
+   </>
        
-    </>
+    
   );
 }
   
